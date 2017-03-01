fd98efe1386544b778702ef6682052bd3651c1b8
Liliana Fiore <liliana.fiorela@gmail.com>
2017-03-01 10:22:23 -0500

    funcion jupiter

--- js/archivo.js
+++ js/archivo.js
@@ -1,10 +1,7 @@
-function pesoPlaneta(){
   function pesoPlaneta(){
 
   var peso = prompt("Ingreso peso: ");
   var jupiter = peso.value * 0.39;
 
   alert("Su Peso en Júpiter es: " + jupiter);
-}
-
 }
