356dbde1d7e206265a962829190933e5e0ef90e7
Miriam Mendoza <miriammp1997@gmail.com>
2017-03-01 10:41:37 -0500

    Listo sin funcion

--- js/archivo.js
+++ js/archivo.js
@@ -1,20 +1,5 @@
-function pesoPlaneta(){
-    
-    var peso = document.getElementById("peso");
-    var jupiter = peso.value * 0.39;
-    var venus   = peso.value * 0.87;
-    var urano   = peso.value * 0.99;
-    var marte   = peso.value * 0.38;
-    var saturno = peso.value * 0.93;
-    var neptuno = peso.value * 1.38;
-        
-    document.getElementById("resultado").innerHTML = "<strong>"+
-                                                      "<ul>" +
-                                                      "<li>" + "Peso en Jupiter: " + jupiter + "</li>" +
-                                                      "<li>" + "Peso en Venus: "+ venus + "</li>" +
-                                                      "<li>" + "Peso en Urano: " + urano + "</li>" +
-                                                      "<li>" + "Peso en Marte: "+marte + "</li>"+
-                                                      "<li>" + "Peso en Saturno: "+saturno + "</li>"+
-                                                      "<li>" + "Peso en Neptuno: "+neptuno + "</li>"+
-                                                      "</strong>";
-}
+var peso = prompt("Ingreso peso: ");
+
+var jupiter = peso * 0.39;
+jupiter.toFixed(2);
+var r = alert("Su Peso en Júpiter es: " + jupiter);
