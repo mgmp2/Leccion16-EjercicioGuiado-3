558e61510462537ace0c25d30f8ed072ad40ea48
Miriam Gisella Mendoza Pilco <Miriam Gisella Mendoza Pilco>
2017-03-04 14:42:35 -0500

    Terminado output

--- js/archivo.js
+++ js/archivo.js
@@ -3,5 +3,15 @@
 var peso = prompt("Ingreso peso: ");
 
 var jupiter = peso * 0.39;
-jupiter.toFixed(2);
-var r = alert("Su Peso en Júpiter es: " + jupiter);
+var marte   = peso * 0.38;
+var saturno = peso * 0.93;
+var neptuno = peso * 1.38;
+var venus   = peso * 0.87;
+var urano   = peso * 0.99;
+
+var r = alert("Peso en Jupiter    :  " + jupiter.toFixed(2) +
+              "\nPeso en Venus    :  " + venus.toFixed(2) +
+              "\nPeso en Marte    :  " + marte.toFixed(2) +
+              "\nPeso en Urano    :  " + urano.toFixed(2) +
+              "\nPeso en Neptuno  :  " + neptuno.toFixed(2) +
+              "\nPeso en Saturno  :  " + saturno.toFixed(2));
